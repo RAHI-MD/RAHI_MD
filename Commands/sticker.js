b314b7bba995b5a64e2a55aecd7952b03d332a2b
Mohammad Rahi <rahiahmed9381@gmail.com>
2026-06-27 23:00:21 +0600

    Update sticker.js

--- Commands/sticker.js
+++ Commands/sticker.js
@@ -36,8 +36,8 @@ async function stickerCommand(sock, chatId, message) {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '120363426138659599@newsletter',
+                    newsletterName: '𝑹𝑨𝑯𝑰_𝑴𝑫',
                     serverMessageId: -1
                 }
             }
@@ -58,8 +58,8 @@ async function stickerCommand(sock, chatId, message) {
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363161513685998@newsletter',
-                        newsletterName: 'KnightBot MD',
+                        newsletterJid: '120363426138659599@newsletter',
+                        newsletterName: '𝑹𝑨𝑯𝑰_𝑴𝑫',
                         serverMessageId: -1
                     }
                 }
@@ -216,8 +216,8 @@ async function stickerCommand(sock, chatId, message) {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '120363426138659599@newsletter',
+                    newsletterName: '𝑹𝑨𝑯𝑰_𝑴𝑫',
                     serverMessageId: -1
                 }
             }
