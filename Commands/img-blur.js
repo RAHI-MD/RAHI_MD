e079448d46e499aff82cad762c642d7f023c4521
Mohammad Rahi <rahiahmed9381@gmail.com>
2026-06-27 22:09:56 +0600

    Update img-blur.js

--- Commands/img-blur.js
+++ Commands/img-blur.js
@@ -65,8 +65,8 @@ async function blurCommand(sock, chatId, message, quotedMessage) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '120363426138659599@newsletter',
+                    newsletterName: '𝑹𝑨𝑯𝑰_𝑴𝑫',
                     serverMessageId: -1
                 }
             }
