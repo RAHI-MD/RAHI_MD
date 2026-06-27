4933e81ce01961738619416c42b0558ef56681d1
Mohammad Rahi <rahiahmed9381@gmail.com>
2026-06-27 16:53:21 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: '𝑹𝑨𝑯𝑰_𝑴𝑫',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "𝑹𝑨𝑯𝑰_𝑴𝑫",
+  botOwner: '𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐑𝐚𝐡𝐢', // Your name
+  ownerNumber: '8801733727775', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.7",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/RAHI-MD/RAHI_MD/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
